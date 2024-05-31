c9d19346e8977fda806341eeda7860caed1645e5
Ishan Hettiarachchi <ishananaguru@gmail.com>
2024-05-31 11:09:08 +0530

    feat: Add data.tsx file with popularPlaces and recommendedPlaces data

---
+++ assets/data/data.tsx
@@ -0,0 +1,91 @@
+import BathTubIcon from "@/components/Icons/BathTubIcon";
+import CutleryIcon from "@/components/Icons/CutleryIcon";
+import PoolIcon from "@/components/Icons/PoolIcon";
+import WifiIcon from "@/components/Icons/WifiIcon";
+
+export enum Facilities {
+  ONE_HEATER = "1 HEATER",
+  DINNER = "DINNER",
+  ONE_TUB = "1 TUB",
+  POOL = "Pool",
+}
+
+export type Facility = {
+  icon: React.ReactNode;
+  facilityName: Facilities;
+};
+
+export type PopularPlace = {
+  title: string;
+  location: string;
+  favorites: boolean;
+  rating: number;
+  number_of_reviews: number;
+  description: string;
+  price: number;
+  facilites: Facility[];
+  image: any;
+};
+
+export const popularPlaces: PopularPlace[] = [
+  {
+    title: "Alley Palace",
+    location: "3818 Grindelwald, Switzerland",
+    favorites: true,
+    rating: 4.1,
+    number_of_reviews: 350,
+    description:
+      "The Alley Palace is a luxurious hotel located in the heart of Grindelwald, Switzerland. The hotel offers a variety of amenities and services to make your stay as comfortable and enjoyable as possible. The hotel is located in a prime location, with easy access to the city's top attractions and landmarks. Whether you're traveling for business or pleasure, the Alley Palace is the perfect choice for your next trip to Grindelwald.",
+    price: 200,
+    facilites: [{ icon: <WifiIcon />, facilityName: Facilities.ONE_HEATER }],
+    image: require("../../assets/images/alley_palace.png"),
+  },
+  {
+    title: "Coeurdes Alpes",
+    location: "3818 Grindelwald, Switzerland",
+    favorites: false,
+    rating: 4.5,
+    number_of_reviews: 355,
+    description:
+      "Aspen is as close as one can get to a storybook alpine town in America. The choose-your-own-adventure possibilities—skiing, hiking, dining shopping and ....",
+    price: 199,
+    facilites: [
+      {
+        icon: <WifiIcon />,
+        facilityName: Facilities.ONE_HEATER,
+      },
+      {
+        icon: <CutleryIcon />,
+        facilityName: Facilities.DINNER,
+      },
+      {
+        icon: <BathTubIcon />,
+        facilityName: Facilities.ONE_TUB,
+      },
+      {
+        icon: <PoolIcon />,
+        facilityName: Facilities.POOL,
+      },
+    ],
+    image: require("../../assets/images/Coeurdes_Alpes.png"),
+  },
+];
+
+export type RecommendedPlace = {
+  title: string;
+  duration: string;
+  image: any;
+};
+
+export const recommendedPlaces: RecommendedPlace[] = [
+  {
+    title: "Explore Aspen",
+    duration: "4N/5D",
+    image: require("../../assets/images/zurich_downtown.png"),
+  },
+  {
+    title: "Luxurious Aspen",
+    duration: "2N/3D",
+    image: require("../../assets/images/the_little_nell_hotel_aspen.jpeg"),
+  },
+];
